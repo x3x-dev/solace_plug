f132f4bc4140a7e805c30225ab4349478391d37f
Priscilla Emasoga <priscillaemasoga@gmail.com>
2025-09-19 19:55:30 +0100

    Export relevant classes through package init files

---
+++ src/solace_plug/publishers/__init__.py
@@ -0,0 +1,3 @@
+from .direct import DirectPublisher, AsyncDirectPublisher
+
+__all__ = ["DirectPublisher", "AsyncDirectPublisher"]
---
+++ src/solace_plug/subscribers/__init__.py
@@ -0,0 +1,3 @@
+from .direct import DirectSubscriber, AsyncDirectSubscriber
+
+__all__ = ["DirectSubscriber", "AsyncDirectSubscriber"]

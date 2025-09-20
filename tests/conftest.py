6ce69d25bf46436773bc9efae99538c528aca035
Priscilla Emasoga <priscillaemasoga@gmail.com>
2025-09-20 22:53:27 +0100

    Handle `SolaceConnectionError` in test fixtures and skip tests if Solace Broker is unavailable

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
 import pytest
 import pytest_asyncio
 from solace_plug.client import SolaceClient, AsyncSolaceClient
-
+from solace_plug.exceptions import SolaceConnectionError 
 
 @pytest.fixture
 def client():
-    with SolaceClient().session() as c:
-        yield c
+    try:
+        with SolaceClient().session() as c:
+            yield c
+    except SolaceConnectionError:
+        pytest.skip("Solace Broker not available")
+
 
 @pytest_asyncio.fixture
 async def async_client():
-    async with AsyncSolaceClient().session() as c:
-        yield c
+    try:
+        async with AsyncSolaceClient().session() as c:
+            yield c
+    except SolaceConnectionError:
+        pytest.skip("Solace Broker not available")
+    

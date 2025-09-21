c6f7e8cdc123cb94d30df290915cc15d5e4a250f
Priscilla Emasoga <priscillaemasoga@gmail.com>
2025-09-21 13:43:14 +0100

    Debug: CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,20 +8,24 @@ log = logging.getLogger("solace_plug")
 
 @pytest.fixture
 def client():
-    try:
-        with SolaceClient().session() as c:
-            yield c
-    except ClientError as e:
-        logging.error("Error connecting to Solace: %s", e)
-        pytest.skip("Solace Broker not available")
-
+    # try:
+    #     with SolaceClient().session() as c:
+    #         yield c
+    # except ClientError as e:
+    #     logging.error("Error connecting to Solace: %s", e)
+    #     pytest.skip("Solace Broker not available")
+    with SolaceClient().session() as c:
+        yield c
+   
 
 @pytest_asyncio.fixture
 async def async_client():
-    try:
-        async with AsyncSolaceClient().session() as c:
-            yield c
-    except ClientError as e:
-        logging.error("Error connecting to Solace: %s", e)
-        pytest.skip("Solace Broker not available")
-    
+    # try:
+    #     async with AsyncSolaceClient().session() as c:
+    #         yield c
+    # except ClientError as e:
+    #     logging.error("Error connecting to Solace: %s", e)
+    #     pytest.skip("Solace Broker not available")
+       
+    async with AsyncSolaceClient().session() as c:
+        yield c
